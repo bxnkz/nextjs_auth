a4d5f6068ee399e1ac0cd319b91c1f2a1654bc3e
bxnkz <bankzzx2@gmail.com>
2024-12-01 19:07:32 +0700

    First

---
+++ src/app/welcome/page.jsx
@@ -0,0 +1,26 @@
+"use client"
+
+import React from 'react'
+import Navbar from '../components/Navbar'
+import { useSession } from 'next-auth/react'
+import { redirect } from 'next/navigation'
+
+function WelcomePage() {
+    const {data: session} = useSession();
+    console.log(session)
+
+    if(!session) redirect("/login");
+  return (
+    <div>
+        <Navbar session={session}/>
+        <div className='container mx-auto'>
+            <h3 className='text-3xl my-3'>Welcome {session?.user?.name}</h3>
+            <p>Email: {session?.user?.email} </p>
+            <hr className='my-3' />
+            <p>Test</p>
+        </div>
+    </div>
+  )
+}
+
+export default WelcomePage
